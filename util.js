ae01df8239bb7995215131ab1596518a7ee43634
caiocosta22 <caiocosta22@gmail.com>
2023-07-11 11:16:06 -0300

    Update funcionando com todas as informações

--- util.js
+++ util.js
@@ -18,4 +18,33 @@
             //canhoto, // ok
             justificativaatraso)*/
 
-            
+        //coalesce(to_char(data_checkin, 'YYYY-MM-DD HH:MI:SS'),'00000000') as data_checkin,
+        //coalesce(to_char(data_entrega, 'YYYY-MM-DD HH:MI:SS'),'00000000') as data_entrega,
+		//coalesce(to_char(data_naoentregue, 'YYYY-MM-DD HH:MI:SS'),'00000000') as data_naoentregue,
+		//coalesce(canhoto,'0') as canhoto,
+        //data_checkin = '${data_checkin}',
+        //data_entrega = '${data_entrega}',
+        //data_naoentregue = '${data_naoentregue}',
+        //canhoto = ${canhoto},
+        
+        /*console.log(conta, // ok
+            //cargaid, // ok
+            //romaneioid, // ok
+            //entidadeid_loja, // ok
+            //motivonaoentregue, // ok
+            //entregue, // ok
+            //data_update_local, // tratar
+            //status_sincronizacao, // ok
+            //latitude_checkin, //tratar
+            //longitude_checkin, //tratar
+            //cidade_checkin, //tratar
+            //estado_checkin, //tratar
+            //cep_checkin, //tratar
+            //rua_checkin, //tratar
+            data_checkin, //tratar
+            data_entrega, //tratar (formatar data)
+            data_naoentregue) // ok
+            //justificativaatraso)
+        //console.log("Oi")
+        //console.log(tamanhoarray)
+        //console.log(x)*/
